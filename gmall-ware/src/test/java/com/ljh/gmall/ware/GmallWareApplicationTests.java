e5505d33cb8175dfc319d74bb71eac697d91c689
ljh <grungust@sina.cn>
2020-04-09 10:49:36 +0800

    项目初始结构创建

---
+++ gmall-ware/src/test/java/com/ljh/gmall/ware/GmallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljh.gmall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
